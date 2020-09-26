c18e1cce14d1de9a16d27ceb8f9a41c1f855c969
cubixprox <61358161+cubixprox@users.noreply.github.com>
2020-09-26 11:07:29 +0530

    Add files via upload

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,15 +1,12 @@
-import axios from 'axios'
-
-// const KEY = "AIzaSyAeDruMhjQGB2noQeW_0lE_iCd856ds3rw";
- const KEY = "AIzaSyAWWrshEL9MCpfKgiNGXmRzsh7nIETmELc"
-
+import axios from "axios";
+import keys from "./keys";
 
 export default axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3",
-    params: {
-        part: 'snippet',
-        maxResult: 5,
-        type: 'video',
-        key: KEY
-    }
-})
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResult: 5,
+    type: "video",
+    key: keys.youtube,
+  },
+});
